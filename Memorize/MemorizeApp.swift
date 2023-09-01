4d1e14abb8386888c8482310eac1ecf5f71eb3e2
lorena-zanferrari <lorena.zanferrari@msn.com>
2023-09-01 16:59:36 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Lorena Zanferrari on 29/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
